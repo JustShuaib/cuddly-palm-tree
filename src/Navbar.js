80e66a46bad2f38763e800799da96d1f20449333
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-06-02 14:17:48 +0100

    added toggle to the nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sass/navbar.scss";
 import hamburger from "./images/icon-hamburger.svg";
 import close from "./images/icon-close.svg";
 function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <nav className="nav">
-      <img src={hamburger} className="hamburger" alt="" />
+      <button
+        aria-label="menu"
+        className="hamburger"
+        onClick={() => setNavOpen(true)}
+      >
+        <img src={hamburger} alt="" />
+      </button>
       <p>room</p>
-      <div>
-        <img src={close} className="close-btn" alt="" />
+      <div className={navOpen ? "nav-list show-nav-list" : "nav-list"}>
+        <button
+          aria-label="close menu"
+          className="close-btn"
+          onClick={() => setNavOpen(false)}
+        >
+          <img src={close} alt="" />
+        </button>
         <ul>
           <li>
             <a href="/">home</a>
